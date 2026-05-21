35f4396f9c1e373a5d64557324d672e9d914fe04
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-05-21 14:06:01 -0300

    Projeto inicial loja virtual ASP.NET MVC

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaVirtual.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
