9ce52f56503b8a6ea6143e5795ecbcb775fe1e51
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-05-28 09:08:11 -0300

    Alteraçoẽs na Home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,27 @@
-using System.Diagnostics;
+using LojaVirtual.Data;
 using Microsoft.AspNetCore.Mvc;
-using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LojaVirtual.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(AppDbContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var products = await _context.Products.ToListAsync();
+
+        return View(products);
     }
 
     public IActionResult Privacy()
     {
         return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
