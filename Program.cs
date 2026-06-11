37da4a07b7e9e4bf9cc755474fe936fadbf8341a
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-06-11 15:39:42 -0300

    Sqlite

--- Program.cs
+++ Program.cs
@@ -8,12 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+/*builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         ServerVersion.AutoDetect(
             builder.Configuration.GetConnectionString("DefaultConnection")
         )
-    ));
+    ));*/
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
